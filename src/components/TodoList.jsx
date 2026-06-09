2ef4c134080de984905722a01adc573548568880
bambxxz1e <s2427@e-mirim.hs.kr>
2026-06-09 09:22:55 +0900

    3208 윤성연 1-8	성장(Tier) ★★ 완료 개수 계산, 조건부 렌더링

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,34 +1,10 @@
-import { useState } from "react"
 import TodoItemEmpty from "./TodoItemEmpty.jsx"
 import TodoItem from "./TodoItem.jsx"
 
 export default function TodoList({ todos, ...rest }) {
-    // 최신순 / 오래된순 상태
-    const [sortType, setSortType] = useState("latest");
-
-    // 정렬된 todos
-    const sortedTodos = [...todos].sort((a, b) => {
-        if (sortType === "latest") {
-            return b.id - a.id // 최신순
-        } else {
-            return a.id - b.id // 오래된순
-        }
-    })
-    
     return (
         <>
-             {/* 정렬 드롭다운 */}
             <div className="todo__header">
-                <div className="todo__sort">
-                    <select
-                        value={sortType}
-                        onChange={(e) => setSortType(e.target.value)}
-                    >
-                        <option value="latest">최신순</option>
-                        <option value="oldest">오래된순</option>
-                    </select>
-                </div>
-
                 <div className="todo__status">
                     <p>완료 {todos.filter(todo => todo.isCompleted).length}개</p>
                 </div>
@@ -40,7 +16,7 @@ export default function TodoList({ todos, ...rest }) {
                 {/* todos가 있으면, TodoItem에 todos 던지기 */}
                 {todos.length > 0 &&
                     // todos에서 하나씩 꺼내서 todo -> <TodoItem todo={todo} />
-                    sortedTodos.map((todo) => <TodoItem key={todo.id} todo={todo} {...rest} />)
+                    todos.map((todo) => <TodoItem key={todo.id} todo={todo} {...rest} />)
                 }
             </ul>
         </>
